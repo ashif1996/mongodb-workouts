63f6005bb36d06964eb90c9aa6fbaef327256e09
Ashif Hassan <ashifhassan1996@gmail.com>
2025-01-01 00:44:58 +0530

    connect a Node.js app to MongoDB using Mongoose

---
+++ workouts/connectDB.js
@@ -0,0 +1,19 @@
+// Connect a Node.js app to MongoDB using Mongoose.
+
+const mongoose = require("mongoose");
+
+const uri = "mongodb://localhost:27017/myDatabase";
+
+const connectToDatabase = async () => {
+    try {
+        const connection = await mongoose.connect(uri);
+        const connectionDetails = `${connection.connection.host}:${connection.connection.port}/${connection.connection.name}`;
+
+        console.log(`MongoDB connected: ${connectionDetails}`);
+    } catch (error) {
+        console.error(`Database connection error: ${error.message}`);
+        process.exit(1);
+    }
+};
+
+connectToDatabase();
